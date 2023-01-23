8103b598d5bdc416e9e089f0fd5694ab3cf84f89
dennisxddxdd <dennisxddxdd@gmail.com>
2023-01-24 00:32:32 +0800

    Update script.js
    Allow easy modification of delay values.

--- script.js
+++ script.js
@@ -13,15 +13,27 @@
     'use strict';
     var counter = 0;
 
+    // Following values are in milliseconds
+    const min_delay_between_poke = 10000;
+    const max_delay_between_poke = 200000;
+    const min_delay_between_cycle = 50000;
+    const max_delay_between_cycle = 100000;
+
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     async function poke() {
         var list = document.querySelectorAll('[aria-label="Poke Back"]');
         for (var i = 0; i < list.length; i++) {
             list[i].click();
-            await sleep(1000+Math.round(Math.random()*20000));
+            await sleep(getRandomInt(min_delay_between_poke, max_delay_between_poke));
         }
     }
 
@@ -30,8 +42,8 @@
             location.reload();
         }
         console.log(counter);
-        var rand = 30000+Math.round(Math.random()*100000);
-        console.log("AutoPoke will be executed after " + rand/1000 + " seconds.");
+        var rand = getRandomInt(min_delay_between_cycle, max_delay_between_cycle);
+        console.log("AutoPoke cycle will be executed after " + rand/1000 + " seconds.");
         setTimeout(function() {
             counter++;
             poke();
